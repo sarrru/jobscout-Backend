91b964bf42fdc21abc7f5b39b60bab7bfd81d293
sarrru <sharusura123@gmail.com>
2025-02-05 21:23:09 +0545

    added missing part

--- controllers/job.controller.js
+++ controllers/job.controller.js
@@ -52,8 +52,10 @@ export const getAllJobs = async (req, res) => {
                 { description: { $regex: keyword, $options: "i" } },
             ]
         };
-        const jobs = await Job.find(query);
-     
+        const jobs = await Job.find(query).populate({
+            path: "company"
+        }).sort({ createdAt: -1 });
+
         if (!jobs || jobs.length === 0) {
             return res.status(404).json({
                 message: "Jobs not found.",
