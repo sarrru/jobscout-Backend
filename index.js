0a499c8761c0d7c3c31ec30fe1febe554b43b765
sarrru <sharusura123@gmail.com>
2025-02-17 22:00:58 +0545

    index.js

--- index.js
+++ index.js
@@ -18,12 +18,16 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-const corsOptions = {
-    origin: 'http://localhost:5173',
-    credentials: true
-}
 
-app.use(cors(corsOptions));
+
+
+// const corsOptions = {
+//     origin: 'http://localhost:5173',
+//     credentials: true
+// }
+
+// app.use(cors(corsOptions));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 const PORT = process.env.PORT || 3000;
 
