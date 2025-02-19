e51c05492dae3c239537c7b0e72b67e8e2345f85
sarrru <sharusura123@gmail.com>
2025-02-19 10:30:02 +0545

    company.route.js

--- routes/company.route.js
+++ routes/company.route.js
@@ -1,13 +1,14 @@
 import express from "express";
-import { getCompany, getCompanyById, registerCompany, updateCompany } from "../controllers/company.controller.js";
 import isAuthenticated from "../middlewars/isAuthenticated.js";
-
+import { getCompany, getCompanyById, registerCompany, updateCompany } from "../controllers/company.controller.js";
+import { singleUpload } from "../middlewars/mutler.js";
 
 const router = express.Router();
 
 router.route("/register").post(isAuthenticated,registerCompany);
 router.route("/get").get(isAuthenticated,getCompany);
 router.route("/get/:id").get(isAuthenticated,getCompanyById);
-router.route("/update/:id").put(isAuthenticated, updateCompany);
+router.route("/update/:id").put(isAuthenticated,singleUpload, updateCompany);
 
 export default router;
+
